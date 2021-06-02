d096090e42332a8c3a80c9d1d4c55956a7c7ee5b
McxWilliam <61747841+McxWilliam@users.noreply.github.com>
2021-06-01 21:13:39 -0300

    Implementação da função truth_value(formula, interpretation).

--- semantics.py
+++ semantics.py
@@ -1,5 +1,6 @@
 """The goal in this module is to define functions associated with the semantics of formulas in propositional logic. """
 
+
 from formula import *
 from functions import atoms
 
@@ -8,8 +9,19 @@ def truth_value(formula, interpretation):
     """Determines the truth value of a formula in an interpretation (valuation).
     An interpretation may be defined as dictionary. For example, {'p': True, 'q': False}.
     """
+    # Implementação da função;
     pass
     # ======== YOUR CODE HERE ========
+    if isinstance(formula, Atom):
+        return interpretation[Atom.__str__(formula)]
+    if isinstance(formula, Not):
+        return not truth_value(formula.inner, interpretation)
+    if isinstance(formula, And):
+        return truth_value(formula.left, interpretation) and truth_value(formula.right, interpretation)
+    if isinstance(formula, Or):
+        return truth_value(formula.left, interpretation) or truth_value(formula.right, interpretation)
+    if isinstance(formula, Implies):
+        return not truth_value(formula.left, interpretation) or (truth_value(formula.right, interpretation))
 
 
 def is_logical_consequence(premises, conclusion):  # function TT-Entails? in the book AIMA.
@@ -35,13 +47,10 @@ def is_satisfiable(formula):
     In other words, if the input formula is satisfiable, it returns an interpretation that assigns true to the formula.
     Otherwise, it returns False."""
     pass
-    # ======== YOUR CODE HERE ========
-
     list_atoms = atoms(formula)
     interpretation = dict()
 
     return sat(formula, list_atoms, interpretation)
-
 
 def sat(formula, list_atoms, interpretation):
     if len(list_atoms) == 0:
@@ -66,3 +75,5 @@ def sat(formula, list_atoms, interpretation):
     if result:
         return result
     return sat(formula, list2, interpretation2)
+
+
