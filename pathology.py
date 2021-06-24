1117821bc676b59c4d1494eaea3974d8371e1bff
McxWilliam <61747841+McxWilliam@users.noreply.github.com>
2021-06-24 20:31:09 -0300

    Atualização Final - parte 1.

--- pathology.py
+++ pathology.py
@@ -5,15 +5,16 @@ import csv
 
 sys.setrecursionlimit(2000)
 
-"""atributos = ['pi<=42.09', 'pi<=48.12', 'pi<=54.92', 'pi<=63.52', 'pi<=70.62', 'pi<=80.61',
+"""
+atributos = ['pi<=42.09', 'pi<=48.12', 'pi<=54.92', 'pi<=63.52', 'pi<=70.62', 'pi<=80.61',
              'pt<=8.31', 'pt<=12.36', 'pt<=14.55', 'pt<=17.44', 'pt<=21.06', 'pt<=28.8',
              'la<=32.59', 'la<=39.63', 'la<=46.33', 'la<=52.68', 'la<=61.27', 'la<=74.10',
              'ss<=29.51', 'ss<=34.38', 'ss<=39.81', 'ss<=44.44', 'ss<=50.55', 'ss<=56.31',
              'rp<=104.70', 'rp<=111.98', 'rp<=116.59', 'rp<=120.08', 'rp<=124.89', 'rp<=130.30',
              'gs<=-0.74', 'gs<=2.10', 'gs<=6.42', 'gs<=25.36', 'gs<=37.89', 'gs<=57.55']
 """
-atributos = ['pi<=42.09','la<=39.63', 'gs<=37.89']
-regras = 1
+atributos = ['pi<=42.09', 'la<=39.63', 'gs<=37.89']
+regras = 2
 
 significado = ['p', 'n', 's']
 
@@ -22,7 +23,7 @@ def lerarquivo(caminho):
     lista = []
     with open(caminho, 'r') as entrada:
         arquivo = csv.reader(entrada)
-        #next(arquivo)
+        next(arquivo)
         for linha in arquivo:
             lista.append(linha)
     return lista
@@ -61,29 +62,29 @@ def or_all(list_formulas):
 def restricao_um(atributos, regras):
     list_and1 = []
     for p in range(regras):
-        list_and2 = []
+        list_and2 = [] # para cada regra, uma nova lista de AND é criada.
         for atr in atributos:
-            list_or = []
-            for r in range(1, 4):
-                if r == 1:
-                    list_or.append(And(And(Atom('X' + atr + ',' + str(r + 1) + ',' + 'p'), Not(Atom('X' + atr + ',' + str(r + 1) + ',' + 'n'))),
-                                        Not(Atom('X' + atr + ',' + str(r + 1) + ',' + 's'))))
-                if r == 2:
-                    list_or.append(And(And(Not(Atom('X' + atr + ',' + str(r + 1) + ',' + 'p')), Atom('X' + atr + ',' + str(r + 1) + ',' + 'n')),
-                                               Not(Atom('X' + atr + ',' + str(r + 1) + ',' + 's'))))
-                if r == 3:
-                    list_or.append(And(And(Not(Atom('X' + atr + ',' + str(r + 1) + ',' + 'p')), Not(Atom('X' + atr + ',' + str(r + 1) + ',' + 'n'))),
-                                               Atom('X' + atr + ',' + str(r + 1) + ',' + 's')))
+            list_or = []  # para cada atributo, uma nova lista de OU será criada.
+
+            list_or.append(And(And(Atom('X' + atr + ',' + str(p + 1) + ',' + 'p'), Not(Atom('X' + atr + ',' + str(p + 1) + ',' + 'n'))),
+                                        Not(Atom('X' + atr + ',' + str(p + 1) + ',' + 's'))))
+
+            list_or.append(And(And(Not(Atom('X' + atr + ',' + str(p + 1) + ',' + 'p')), Atom('X' + atr + ',' + str(p + 1) + ',' + 'n')),
+                                               Not(Atom('X' + atr + ',' + str(p + 1) + ',' + 's'))))
+
+            list_or.append(And(And(Not(Atom('X' + atr + ',' + str(p + 1) + ',' + 'p')), Not(Atom('X' + atr + ',' + str(p + 1) + ',' + 'n'))),
+                                               Atom('X' + atr + ',' + str(p + 1) + ',' + 's')))
             or_form = or_all(list_or)
             list_and2.append(or_form)
         list_and1.append(and_all(list_and2))
+
 
     return and_all(list_and1)
 
 def restricao_dois(atributos, regras):
     list_and = []
     for i in range(regras):
-        list_or = []
+        list_or = [] # para cada atributo, uma nova lista de OU será criada.
         for atr in atributos:
             list_or.append(Not(Atom('X' + atr + ',' + str(i+1) + ',' + 's')))
 
@@ -95,22 +96,22 @@ def restricao_dois(atributos, regras):
 def restricao_tres(atributos, regras):
     datas = lerarquivo('arquivos/column_bin_compacto.csv')
     list_and1 = []
-    for linha in datas:
-        if linha[3] == '0':
-            list_and2 = []
+    for paciente in datas:
+        if paciente[3] == '0':
+            list_and2 = [] # para cada nova linha do arquivo, uma nova lista de AND é criada.
             for r in range(regras):
                 list_or = []
-                for i in range(3):
-                    if linha[i] == '0':
+                for i in range(len(paciente) - 1): # quantidade de colunas do arquivo compactado;
+                    if paciente[i] == '0':
                         list_or.append(Atom('X' + atributos[i] + ',' + str(r + 1) + ',' + 'p'))
-                    elif linha[i] == '1':
+                    elif paciente[i] == '1':
                         list_or.append(Atom('X' + atributos[i] + ',' + str(r + 1) + ',' + 'n'))
-
 
                 or_form = or_all(list_or)
                 list_and2.append(or_form)
             and_form = and_all(list_and2)
             list_and1.append(and_form)
+
     return and_all(list_and1)
 
 def restricao_quatro(atributos, regras):
@@ -122,7 +123,7 @@ def restricao_quatro(atributos, regras):
             list_implies = []
             if paciente[3] == '1':
                 enfermo += 1
-                for j in range(3):
+                for j in range(len(paciente) - 1):
                     if paciente[j] == '0':
                         list_implies.append(Implies(Atom('X' + atributos[j] + ',' + str(r+1) + ',' + 'p'),
                                                     Not(Atom('C' + str(r+1) + ',' + str(enfermo)))))
@@ -131,6 +132,7 @@ def restricao_quatro(atributos, regras):
                                                     Not(Atom('C' + str(r + 1) + ',' + str(enfermo)))))
                 and_form = and_all(list_implies)
                 list_and.append(and_form)
+
     return and_all(list_and)
 
 def restricao_cinco(regras):
@@ -166,16 +168,24 @@ def pathology_solution(atributos, regras):
     solution = is_satisfiable(final_formula)
     if solution:
         print(solution)
+        rules = defined_attribute(solution) # matriz em que cada elemento é uma regra;
+        print("-- Regras --")
+        for rule in rules:
+            print(rule)
+        taxa_acerto = porcentege_of_correct_answers(rules)
+        print(f"Taxa de acerto - {taxa_acerto}%")
     else:
-        print("Insatisfatível")
-
-#pathology_solution(atributos, regras)
+        print("Insatisfatível.")
 
 
-print(restricao_um(atributos, regras))
+def defined_attribute(solution):
+    list_atoms = []
+    for k, v in solution.items():
+        if(v == True):
+            list_atoms.append(k)
+    return make_rules(list_atoms)
 
-
-
+pathology_solution(atributos, regras)
 
 
 
